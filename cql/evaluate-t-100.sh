2714766dc90e2f90dd894376dbce60bd3005670a
Alexander Kiel <alexanderkiel@gmx.net>
2021-02-02 19:40:13 +0100

    T-100

--- cql/evaluate-t-100.sh
+++ cql/evaluate-t-100.sh
@@ -8,11 +8,26 @@ echo "T100 - Geschlecht"
 echo "T100 - Geschlecht + Laborwert Covid Positiv"
 ./evaluate-measure.sh -f t-100/t-100-2.cql ${BASE}
 
-echo "T100 - Geschlecht + Laborwert Covid Positiv + Laborwert Hemoglobin"
-./evaluate-measure.sh -f t-100/t-100-3.cql ${BASE}
-
 echo "T100 - Geschlecht + Laborwert Covid Positiv + Laborwert Hemoglobin (ohne Zeit)"
 ./evaluate-measure.sh -f t-100/t-100-3.cql ${BASE}
 
 echo "T100 - Geschlecht + Laborwert Covid Positiv + Laborwert Hemoglobin (mit Zeit)"
 ./evaluate-measure.sh -f t-100/t-100-3-time.cql ${BASE}
+
+echo "T100 - Geschlecht + Laborwert Covid Positiv + Laborwert Hemoglobin (ohne Zeit) + Diagnose Brustkrebs"
+./evaluate-measure.sh -f t-100/t-100-4.cql ${BASE}
+
+echo "T100 - Geschlecht + Laborwert Covid Positiv + Laborwert Hemoglobin (mit Zeit) + Diagnose Brustkrebs"
+./evaluate-measure.sh -f t-100/t-100-4-time.cql ${BASE}
+
+echo "T100 - Geschlecht + Laborwert Covid Positiv + Laborwert Hemoglobin (ohne Zeit) + Diagnose Brustkrebs + Bioprobe"
+./evaluate-measure.sh -f t-100/t-100-5.cql ${BASE}
+
+echo "T100 - Geschlecht + Laborwert Covid Positiv + Laborwert Hemoglobin (mit Zeit) + Diagnose Brustkrebs + Bioprobe"
+./evaluate-measure.sh -f t-100/t-100-5-time.cql ${BASE}
+
+echo "T100 - Geschlecht + Laborwert Covid Positiv + Laborwert Hemoglobin (ohne Zeit) + Diagnose Brustkrebs + Bioprobe + Prozedur Bluttransfusion (ohne Zeit)"
+./evaluate-measure.sh -f t-100/t-100-6.cql ${BASE}
+
+echo "T100 - Geschlecht + Laborwert Covid Positiv + Laborwert Hemoglobin (mit Zeit) + Diagnose Brustkrebs + Bioprobe + Prozedur Bluttransfusion (mit Zeit)"
+./evaluate-measure.sh -f t-100/t-100-6-time.cql ${BASE}
